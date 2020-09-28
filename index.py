be26076ad1cb817bdc60feda59079e103778592a
ohata20 <ohata20@sip.tuat.ac.jp>
2020-09-28 12:15:07 +0900

    second

--- index.py
+++ index.py
@@ -1,7 +1,28 @@
-from bottle import route, run, template
+from bottle import route, run, template, static_file
+from bottle import TEMPLATE_PATH
+import os
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+TEMPLATE_PATH.append("./view")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, 'view')
+
+@route('/view/css/<filename:path>')
+def send_static(filename):
+    """静的ファイルを返す
+    """
+    return static_file(filename, root=f'{STATIC_DIR}/css')
+
+
+
+@route('/')
+def index():
+	title="aaaa"
+	return template('index_e.html',title=title)
+
+@route('/', method='POST')
+def index2():
+	return template("index2.html")
+
+
 
 run(host='localhost', port=8080)
